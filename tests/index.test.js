333e2472fb6b5c8a5613930d44c0b3280f8273ef
shivanandu <shivanandu.dev@gmail.com>
2025-11-09 20:44:13 +0530

    test: add user signup validation test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,27 @@
-// test 
 function sum(a, b) {
     return a+b
 }
-test('adds 1+2 to be equal 3',()=>{
-    let ans = sum(1,2);
-    expect(ans).toBe(3);
+const BACKEND_URL="http://localhost:3000";
+describe("Authentication", ()=>{
+test('User is able to sign up',async() =>{
+   const usernme="snk"+Math.random();
+   const password = "123456";
+   const response= await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+    username,
+    password,
+    type:"admin"
+   })
+
+   expect(response.statusCode).toBe(200)
+
+ const updatedResponse= await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+    username,
+    password,
+    type:"admin"
+   })
+
+expect(updatedResponse.statusCode).toBe(400);
+
 });
+})
+
