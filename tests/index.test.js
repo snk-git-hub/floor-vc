91396cb3e268d94c7f757f917501055cd7b10774
shivanandu <shivanandu.dev@gmail.com>
2025-11-12 11:35:02 +0530

    test:User can and can't  update their metadata with the right avathar id

--- tests/index.test.js
+++ tests/index.test.js
@@ -64,3 +64,54 @@ test(`Signin fails if the username and password are incorrect`,async()=>{
   })
 });
 
+
+describe("User Information endpoint",()=>{
+   let token = "";
+   let avatharId="";
+
+   beforeAll(async ()=>{
+      const username = `snk-${Math.random()}`
+      const password = "123456"
+   const response_=await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+         username,
+         password,
+         type:"admin"
+      })
+
+   const response =await axios.post(`${BACKEND_URL}/api/v1/signin`,{
+         username,
+         password,
+      })
+
+      token = response.data.token
+
+
+      const avatharResponse = await axios.post(`${BACKEND_URL}/api/v1/avatar`,{
+         "imageUrl":"https://pin.it/3orGVA49C",
+         "name":"Timmy"
+      })
+      avatharId= avatharResponse.avatharId;
+
+   })
+  test("User can't update their metadata with a wrong avatar id",async()=>{
+   const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`,{
+      avatharId:"123123123"
+   },{
+      headers:{
+         "authorization":`Bearer ${token}`
+      }
+   })
+   expect(response.statusCode).toBe(400);
+  })
+   test("User can update their metadata with the right avathar id",async()=>{
+         const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadat`,{
+            avatharId
+         },{
+            headers:{
+               "authorization":`Bearer${token}`
+            }
+         })
+         test(response.statusCode).toBe(200)
+   })
+})
+
